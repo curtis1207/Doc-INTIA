420016fdc237e719a5deafed321614fe9b117abd
curtis1207 <75917677+curtis1207@users.noreply.github.com>
2025-04-09 18:58:56 +0200

    Add files via upload

---
+++ INTIA/INTIA/settings.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+import os
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+SECRET_KEY = 'django-insecure-change-me'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+     'gestion',  # notre app principale
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+ROOT_URLCONF = 'INTIA.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+       'OPTIONS': {
+    'context_processors': [
+        'django.template.context_processors.debug',
+        'django.template.context_processors.request',  # <--- important !
+        'django.contrib.auth.context_processors.auth',  # <--- important !
+        'django.contrib.messages.context_processors.messages',  # <--- important !
+    ],
+},
+    },
+]
+WSGI_APPLICATION = 'INTIA.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = []
+
+LANGUAGE_CODE = 'fr-fr'
+TIME_ZONE = 'Africa/Douala'
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = '/static/'
---
+++ INTIA/gestion/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Agence, Client, Assurance
+
+admin.site.register(Agence)
+admin.site.register(Client)
+admin.site.register(Assurance)
